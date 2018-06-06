b5b250bf3747d44754a215afebf6d4e50d413841
brian <trangb@oregonstate.edu>
2018-06-05 18:15:07 -0700

    got plaintext transfer to work

--- otp_enc.c
+++ otp_enc.c
@@ -7,6 +7,8 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 #include <fcntl.h> //allows files opening, reading, writing
+#include <stdint.h>
+#include <errno.h>
 /*
 *
 *This program connects to otp_enc_d, 
@@ -20,7 +22,7 @@
 #define LOWERASCII 65      //A
 #define HIGHERASCII 90		//Z
 #define SPACEASCII 32		//(space)
-
+#define PACKSIZE 500
 
 void error(const char *msg) { 
 	perror(msg); exit(0); 
@@ -31,18 +33,17 @@ void error(const char *msg) {
 *
 */
 
-int checkFile(char * plaintextfile,char*buffer) {
+int checkFile(char * fileName,char*buffer) {
 	FILE * pFile;
 	int plaintextfd;
 	int plaintext_size;
 	plaintext_size = 0;
 
-	int charGrabbed;
 	int charIndex;
 	charIndex = 0;
 
 	//open the file
-	pFile = fopen(plaintextfile, "r");
+	pFile = fopen(fileName, "r");
 	//error checking for open
 	if (pFile == NULL) {
 		perror("error opening file");
@@ -94,11 +95,58 @@ void compareKeyAndPlainText(int keySize, int plainSize,char*keyFile) {
 	}
 
 }
+
+//void sendFile(int sockfd, char* filename,int filelength) {
+//	FILE * pFile;
+//	char buffer[TOTALCHARS];
+//
+//	//open the file
+//	pFile = fopen(fileName, "r");
+//	//error checking for open
+//	if (pFile == NULL) {
+//		perror("error opening file");
+//	}//keep reading until end of file
+//
+//	do {
+//		int cInt;
+//		char c = fgetc(pFile);
+//		// Checking for end of file
+//		if (feof(pFile))
+//			break;
+//		cInt = c;
+//
+//		//skip newline
+//		if (c == '\n') {
+//
+//			fflush(stdout);
+//			break;
+//		}
+//
+//
+//
+//
+//		buffer[charIndex++] = c;
+//
+//		plaintext_size++;
+//	} while (1);
+//
+//}
+
 /***
 *
 *SOCKET PREP STUFF
 *
 */
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
+	}
+
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
 
 int attemptConnection(struct hostent * server,char* port) {
 	struct sockaddr_in serverAddress;
@@ -140,11 +188,100 @@ int attemptConnection(struct hostent * server,char* port) {
 	return sockfd;
 
 }
+//returns sock for comm
+int connectToOTP(char*port,struct addrinfo *servinfo) {
+	int sockfd, numbytes;
+	int rv;
+	struct addrinfo hints;
+	struct addrinfo *p;
+	char s[INET6_ADDRSTRLEN];
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
 
+	if ((rv = getaddrinfo("127.0.0.1", port, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		exit(1);
+	}
+
+	//loop through all results and connect to the first we can
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+			p->ai_protocol)) == -1) {
+			perror("client: socket");
+			continue;
+		}
+
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("client: connect");
+			continue;
+		}
+
+		break;
+	}
+	if (p == NULL) {
+		fprintf(stderr, "client: failed to connect\n");
+		exit(2);
+	}
+
+	inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+		s, sizeof s);
+	printf("client: connecting to %s\n", s);
+	return sockfd;
+}
+
+// 's' is socket you want to send to
+// 'buf' is the buffer containing data
+//len is pointer to an int container the number of bytes in buffer
+//beej guide
+int sendall(int s, char *buf, int *len)
+{
+	int total = 0;        // how many bytes we've sent
+	int bytesleft = *len; // how many we have left to send
+	int n;
+
+	while (total < *len) {
+		n = send(s, buf + total, bytesleft, 0);
+		if (n == -1) { break; }
+		total += n;
+		bytesleft -= n;
+	}
+
+	*len = total; // return number actually sent here
+
+	return n == -1 ? -1 : 0; // return -1 on failure, 0 on success
+}
+//SOURCE https://stackoverflow.com/questions/9140409/transfer-integer-over-a-socket-in-c
+int send_int(int num, int fd)
+{
+	int32_t conv = htonl(num);
+	char *data = (char*)&conv;
+	int left = sizeof(conv);
+	int rc;
+	do {
+		rc = write(fd, data, left);
+		if (rc < 0) {
+			if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+				// use select() or epoll() to wait for the socket to be writable again
+			}
+			else if (errno != EINTR) {
+				return -1;
+			}
+		}
+		else {
+			data += rc;
+			left -= rc;
+		}
+	} while (left > 0);
+	return 0;
+}
+
+//"127.0.0.1" localhost
 int main(int argc, char*argv[]) {
 	
-	int socketFD, portNumber, charsWritten, charsRead;
-
+	int socketFD;
+	struct addrinfo *servinfo;
 	
 	//store server data
 	struct hostent *server;
@@ -165,83 +302,56 @@ int main(int argc, char*argv[]) {
 	} // Check usage & args
 
 	plaintext = argv[1];
-	printf("plaintext= %s\n", plaintext);
 	keyfile = argv[2];
-	printf("keyfile= %s\n\n", keyfile);
-	printf("port is = %s\n\n", argv[3]);
 	//check plaintext file
 	plaintext_size=checkFile(plaintext,bufferPlainText);
-
-
+	printf("bufferPlainText = %s\n", bufferPlainText);
+	printf("strlen(bufferPlainText) = %d\n", strlen(bufferPlainText));
 	//check keyfile
 	keytext_size = checkFile(keyfile, bufferKeyText);
-
-
+	printf("bufferKeyText = %s\n", bufferKeyText);
+	printf("strlen(bufferKeyText) = %d\n", strlen(bufferKeyText));
 
 	//compare keyfile and plaintextfile lengths
 	compareKeyAndPlainText(keytext_size, plaintext_size, keyfile);
 
-	socketFD = attemptConnection(server, argv[3]);
+	//socketFD = attemptConnection(server, argv[3]);
+	socketFD=connectToOTP(argv[3], servinfo);
 
 
-
-	//send confirm to opt_enc_d.c
 	int n;
 
 	char*confirm = "otp_enc";
 	n = write(socketFD, confirm, strlen(confirm));
-	if (n < 0) {
-		error("WRITING TO SOCKET CREATED AN ERROR");
+
+	char ack[PACKSIZE];
+	memset(ack, 0, sizeof(ack));
+	n = read(socketFD, ack, sizeof(ack));
+
+	if (strcmp(ack, confirm) == 0) {
+		printf("confirmed correct\n");
+		fflush(stdout);
+	}
+	else {
+		fprintf(stderr, "ERROR: could not contact otp_enc_d\n");
+		exit(2);
 	}
 
+	//send plaintext to otp_enc_d
+	//sendall(socketFD,)
+
+	//send size of file to otp_enc_d
+	n = send(socketFD, &plaintext_size, sizeof(plaintext_size), 0);
+	//send_int(plaintext_size, socketFD);
+
+
+	n = sendall(socketFD, bufferPlainText, &plaintext_size);
 
 
 
 
 
 
-
-
-
-
-
-
-
-	
-	//close after sending msg
-	printf("closing socket\n");
-	fflush(stdout);
-	close(socketFD);
-
-
-	//printf("plaintext_size= %d\n\n", plaintext_size)
-
-	
-	//memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
-	//portNumber = atoi(argv[3]); // Get the port number, convert to an integer from a string
-	//printf("portNumber of otp_enc is %d\n", portNumber);
-
-
-	//serverAddress.sin_family = AF_INET; // Create a network-capable socket
-	//serverAddress.sin_port = htons(portNumber); // Store the port number
-	//serverHostInfo = gethostbyname("localhost"); // Convert the machine name into a special form of address
-
-	//printf("host name is %s\n", serverHostInfo->h_name);
-	//if (serverHostInfo == NULL) {
-	//	fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0);
-	//}
-
-	////prserve the special arrangement of the bytes in these variables by copying the bytes 
-	////in the given order, regardless of format,structure,
-	//memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
-
-	//// Set up the socket
-	//socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
-	//if (socketFD < 0) error("CLIENT: ERROR opening socket");
-
-	//// Connect to server
-	//if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
-	//	error("CLIENT: ERROR connecting");
 
 
 }
--- otp_enc_d.c
+++ otp_enc_d.c
@@ -11,6 +11,7 @@
 #include <fcntl.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <stdint.h>
 //this program will run as daemon in background
 //upon execution, must output an error if it cannot be run due to a network error
 
@@ -32,9 +33,9 @@
 //five concurrent socket connections running at same time
 #define MAXPROCESSES 5
 #define PACKSIZE 500
-
-
-
+#define TOTALFORKS 50
+#define BACKLOG 5
+#define TOTALCHARS 70002
 //pid data structure
 struct pidarray {
 	int pidcount;
@@ -45,7 +46,7 @@ struct pidarray {
 //GLOBALS
 struct sigaction SIGCHLD_action;
 struct pidarray pstack;
-
+int numforks;
 
 
 void pidConstructor() {
@@ -92,6 +93,9 @@ void error(const char*msg) {
 	exit(1);
 }
 
+int generateNewPort() {
+
+}
 
 
 
@@ -99,50 +103,253 @@ void error(const char*msg) {
 void handle_sigchld(int sig) {
 	int saved_errno = errno;
 	pid_t dead;
+	char * die = "dead";
 	while (dead = waitpid((pid_t)(-1), 0, WNOHANG) > 0) {
-		removepid(dead);
+		//removepid(dead);
+		//write(STDOUT_FILENO, die, 4);
+		numforks--;
 	}
 	errno = saved_errno;
 }
+//SOURCE https://stackoverflow.com/questions/9140409/transfer-integer-over-a-socket-in-c
+int receive_int(int *num, int fd)
+{
+	int32_t ret;
+	char *data = (char*)&ret;
+	int left = sizeof(ret);
+	int rc;
+	do {
+		rc = read(fd, data, left);
+		if (rc <= 0) { /* instead of ret */
+			if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+				// use select() or epoll() to wait for the socket to be readable again
+			}
+			else if (errno != EINTR) {
+				return -1;
+			}
+		}
+		else {
+			data += rc;
+			left -= rc;
+		}
+	} while (left > 0);
+	*num = ntohl(ret);
+	return 0;
+}
+
 /*
 *
 *SOCKET FUNCTIONS
 *
 */
 
-
-//initialize socket
-int initSocket() {
-	int sockfd;
-
-	 sockfd= socket(AF_INET, SOCK_STREAM, 0);
-
-	 if (sockfd < 0) {
-		 error("ERROR opening socket");
-	 }
-	return sockfd;
-}
-
-
-//prepare server socket
-void serverSocketPrep(int sockfd, struct sockaddr_in serverAddress) {
-
-	int bindResult;
-	if (bind(sockfd, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
-		error("ERROR on binding");
+//from BEEJ GUIDE
+//void setUPConnection(struct addrinfo *servinfo,char*port) {
+//	struct addrinfo hints;
+//	struct sockaddr_storage their_addr;		//connector's address info
+//	socklen_t sin_size;
+//	int rv;
+//	memset(&hints, 0, sizeof hints);
+//	hints.ai_family = AF_UNSPEC;
+//	hints.ai_socktype = SOCK_STREAM;
+//	hints.ai_flags = AI_PASSIVE;		//use my IP
+//
+//
+//	if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
+//		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+//		exit(1);
+//	}
+//}
+//from BEEJ GUIDE
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+	if (sa->sa_family == AF_INET) {
+		return &(((struct sockaddr_in*)sa)->sin_addr);
 	}
 
-	int listenResult;
+	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+//from BEEJ GUIDE
+int setUPConnection(char*port,struct addrinfo *servinfo) {
 
-	listenResult = listen(sockfd, 5);
+	struct addrinfo hints;
+	struct sockaddr_storage their_addr;		//connector's address info
+	socklen_t sin_size;
+	int rv;
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;		//use my IP
 
-	if (listenResult == -1) {
-		close(sockfd);
-		fprintf(stderr, "Error listening.\n");
+
+	if ((rv = getaddrinfo(NULL, port, &hints, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		exit(1);
 	}
 
+
+	struct addrinfo *p;
+	int sockfd;
+	int yes = 1;
+	for (p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+			perror("server:socket");
+			continue;
+		}
+
+		if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+			perror("setsockopt");
+			exit(1);
+		}
+		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			perror("server: bind");
+			continue;
+		}
+		break;
+	}
+	freeaddrinfo(servinfo);
+	if (p == NULL) {
+		fprintf(stderr, "server: failed to bind\n");
+		exit(1);
+	}
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+	return sockfd;
+
 }
+//from BEEJ GUIDE
+int recvall(int s, char *buf, int *len)
+{
+	int total = 0;        // how many bytes we've sent
+	int bytesleft = *len; // how many we have left to send
+	int n;
+
+	while (total < *len) {
+		n = recv(s, buf + total, bytesleft, 0);
+		if (n == -1) { break; }
+		total += n;
+		bytesleft -= n;
+	}
+
+	*len = total; // return number actually sent here
+
+	return n == -1 ? -1 : 0; // return -1 on failure, 0 on success
+}
+//
+//only in the child process will the actual encryption take place, 
+//and the ciphertext be written back :
+void childActivity(int sockfd) {
+
+	//end of signal stuff
+
+
+	int n;
+	int plainSize;
+	char buffer[TOTALCHARS];
+	memset(buffer, 0, sizeof buffer);
+	//we now know length of plainsize
+	n = recv(sockfd, &plainSize, 4, 0);
+	if (n == -1) {
+		fprintf(stderr, "ERROR recv plain size\n");
+		exit(1);
+	}
+
+	printf("plainSize %d\n", plainSize);
+	
+
+	//need to recv plainsize file
+	n = recvall(sockfd, buffer, &plainSize);
+	printf("strlen(buffer) %d\n", buffer);
+	if (n == -1) {
+		fprintf(stderr, "ERROR recvall plainsize content\n");
+		exit(1);
+
+	}
+	else {
+		printf("plain file received is %s\n", buffer);
+	}
+	
+}
+//from BEEJ GUIDE
+void handleProcesses(int sockfd) {
+	char * confirm = "otp_enc";
+
+	
+	char s[INET6_ADDRSTRLEN];
+	socklen_t sin_size;
+	struct sockaddr_storage their_addr; // connector's address information
+	int new_fd;
+	pid_t spawn;
+	
+	numforks = 0;
+	while (1) {
+		char message[PACKSIZE];
+		memset(message, 0, sizeof message);
+		int bytesRECV;
+		int bytesSENT;
+		char msg[1024];
+		memset(msg, 0, sizeof msg);
+		sin_size = sizeof their_addr;
+		new_fd = accept(sockfd, (struct sockaddr*)&their_addr, &sin_size);
+		if (new_fd == -1) {
+			//perror("ERROR on accept");
+			continue;
+		}
+		inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
+		printf("server: got connection from %s\n", s);
+		fflush(stdout);
+		int n;
+		n = read(new_fd, message, sizeof(message));
+		printf("message %s\n", message);
+		//confirm its otp_enc before forking
+		if (strcmp(message, confirm) == 0 && (numforks<5)) {
+			//inc numforks
+			numforks++;
+			printf("communicating with the right process\n");
+			fflush(stdout);
+
+			
+			n = write(new_fd, confirm, sizeof(confirm));
+
+			if (spawn = fork() == 0) {
+				printf("inside child of otp_enc_d\n");
+				fflush(stdout);
+
+				close(sockfd);
+
+				//do stuff
+				childActivity(new_fd);
+
+				_exit(0);
+			}
+			
+			//parent doesn't need this
+			close(new_fd);
+		}
+		else {
+			if (numforks > 5) {
+				printf("Too many requests\n");
+				fflush(stdout);
+				continue;
+			}
+			char * error = "error";
+			int n;
+			n = write(new_fd, error, sizeof(error));
+			continue;
+		}
+
+
+		
+	}
+}
+
+
+
 /*
 *
 *END OF SOCKET FUNCTIONS
@@ -152,6 +359,22 @@ void serverSocketPrep(int sockfd, struct sockaddr_in serverAddress) {
 // runs like otp_enc_d 51002 &
 int main(int argc, char*argv[]) {
 
+	SIGCHLD_action.sa_handler = &handle_sigchld;	//reap children
+	sigemptyset(&SIGCHLD_action.sa_mask);
+
+
+	//DO I NEED  SA_NOCLDSTOP???
+	SIGCHLD_action.sa_flags = SA_RESTART | SA_NOCLDSTOP;
+	//associate SIGCHLD with handle_sigchld
+	if (sigaction(SIGCHLD, &SIGCHLD_action, 0) == -1) {
+		perror(0);
+		exit(1);
+	}
+
+	//for port generation
+	srand(time(0));
+	struct addrinfo *servinfo;
+	int sockfd;
 	if (argc < 2) {
 		fprintf(stderr, "Make sure you enter a port #\n", argv[0]);
 		exit(1);
@@ -162,23 +385,22 @@ int main(int argc, char*argv[]) {
 	/*
 	**SIGNAL STUFF
 	*/
-	SIGCHLD_action.sa_handler = &handle_sigchld;
-	sigemptyset(&SIGCHLD_action.sa_mask);
-	SIGCHLD_action.sa_flags = SA_RESTART | SA_NOCLDSTOP;
-	//associate SIGCHLD with handle_sigchld
-	if (sigaction(SIGCHLD, &SIGCHLD_action, 0) == -1) {
-		perror(0);
-		exit(1);
-	}
 
-	//end of signal stuff
 	/*
 	** END OF SIGNAL STUFF
 	*
 	*/
 
+
+	sockfd=setUPConnection(argv[1],servinfo );
+	handleProcesses(sockfd);
+
+	//sockfd =bindLoopSockets(servinfo);
+
+
+
 	int portNum;
-	int sockfd;
+
 	int establishedConnectionFD;
 	int charsRead;
 	int socketConns[MAXPROCESSES];
@@ -186,22 +408,18 @@ int main(int argc, char*argv[]) {
 	socklen_t sizeofClientInfo;
 	char buffer[256];
 	//sockaddr_in declarations and inits
-	struct sockaddr_in serverAddress, clientAddress;
-	//SOCKET INITS
-	memset((char*)&serverAddress, '\0', sizeof(serverAddress));
-	portNum = atoi(argv[1]);					//change char into int
-	serverAddress.sin_family = AF_INET;			//create a network capable socket
-	serverAddress.sin_port = htons(portNum);
-	serverAddress.sin_addr.s_addr = INADDR_ANY;
+	//struct sockaddr_in serverAddress, clientAddress;
+	//memset((char*)&serverAddress, '\0', sizeof(serverAddress));
+	//portNum = atoi(argv[1]);					//change char into int
+	//serverAddress.sin_family = AF_INET;			//create a network capable socket
+	//serverAddress.sin_port = htons(portNum);
+	//serverAddress.sin_addr.s_addr = INADDR_ANY;
 	//END OF SOCKET INITS
 
-	sockfd = initSocket();
+	//sockfd = initSocket();
 
 
-	serverSocketPrep(sockfd, serverAddress);
-
-
-
+	//serverSocketPrep(sockfd, serverAddress);
 
 
 
@@ -209,67 +427,49 @@ int main(int argc, char*argv[]) {
 
 
 
-	int listenStatus;
-	listenStatus = listen(sockfd, 5);
+
+
+
+	//int listenStatus;
+	//listenStatus = listen(sockfd, 5);
 
 	//server runs forever
-	while (1) {
-		//variable declaration for possible child process
-		pid_t spawnPid = -5;
-		sizeofClientInfo = sizeof(clientAddress);
+	//while (1) {
+	//	//variable declaration for possible child process
+	//	pid_t spawnPid = -5;
+	//	sizeofClientInfo = sizeof(clientAddress);
 
-		//accept incoming connections
-		establishedConnectionFD = accept(sockfd, (struct sockaddr *)&clientAddress, &sizeofClientInfo);
-		if (establishedConnectionFD < 0) {
-			error("ERROR on accept");
-		}//successful connection
-		//in terms of creating that child process as described above, you may either create with fork
-		else {
-			printf("accept connection successful\n");
-			fflush(stdout);
-			char message[500];
-			memset(message, '\0', sizeof(message));
-			int n;
-			//try to receive "otp_enc" so we know its the correct program
-			char*confirm = "otp_enc";
-			//n = read(establishedConnectionFD, message, sizeof(message));
+	//	//accept incoming connections
+	//	establishedConnectionFD = accept(sockfd, (struct sockaddr *)&clientAddress, &sizeofClientInfo);
+	//	if (establishedConnectionFD < 0) {
+	//		error("ERROR on accept");
+	//	}//successful connection
+	//	//in terms of creating that child process as described above, you may either create with fork
+	//	else {
 
-			//n = write(establishedConnectionFD, returnMsg, strlen(returnMsg));
-			n = read(establishedConnectionFD, message, sizeof(message));
-			if (strcmp(message, confirm) == 0) {
-				printf("This is the right program\n");
-				fflush(stdout);
-			}
-			else {
-				printf("This is the wrong program\n");
-				fflush(stdout);
+	//		printf("accept connection successful\n");
+	//		fflush(stdout);
+	//		char message[500];
+	//		memset(message, '\0', sizeof(message));
+	//		int n;
+	//		//try to receive "otp_enc" so we know its the correct program
+	//		char*confirm = "otp_enc";
+	//		n = read(establishedConnectionFD, message, sizeof(message));
+	//		if (strcmp(message, confirm) == 0) {
+	//			printf("This is the right program\n");
+	//			fflush(stdout);
+	//		}
+	//		else {//HANDLES WRONG PROGRAM
+	//			printf("This is the wrong program\n");
+	//			fflush(stdout);
 
-			}
+	//		}
 
-			
-			//spawnPid = fork();
-			//switch (spawnPid)
-			//{
-			//	case -1: {
-			//		perror("Hull Breach!\n");
-			//		exit(1);
-			//		break;
-			//	}
-			//		 //the child process of otp_enc_d must first check to make sure it is communicating with otp_enc
-			//	case 0: {//only in the child process the the actual encryption takes place
+	//	}
+
+	//}
 
 
-			//		break;
-			//	}
 
-			//	default:
-			//	{
-			//		addPid(spawnPid);
-			//		break;
-			//	}
 
-			//}//END OF SWITCH FORK
-		}
-
-	}
 }
